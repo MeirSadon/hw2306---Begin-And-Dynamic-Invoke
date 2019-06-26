d5ad54fa684baedde26448661b2e4ac64f8eaac2
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-06-26 16:59:06 +0300

    Add files via upload

---
+++ hw2306 - TaskGetPie - Exrice1/hw2306 - TaskGetPie - Exrice1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2306___TaskGetPie___Exrice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<float> myFunc = new Func<float>(GetPie);
+            Task.Run(() => { Console.WriteLine(myFunc()); });
+            Console.ReadLine();
+        }
+        static float GetPie()
+        {
+            return 3.14f;
+        }
+    }
+}
---
+++ hw2306 - delagateWithStringFunction - Exrice2/hw2306 - delagateWithStringFunction - Exrice2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2306___delagateWithStringFunction___Exrice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string> addWords = new Func<string>(GetWord1);
+            addWords += GetWord2;
+            addWords += GetWord3;
+
+            string sentence = "";
+
+
+            //way 1 - Begin And End Invoke.
+            //foreach (Func<string> word in addWords.GetInvocationList())
+            //{
+            //   IAsyncResult ar = word.BeginInvoke(null,null);
+            //   sentence += word.EndInvoke(ar);
+            //}
+            //Console.WriteLine(PrintTheSentence(sentence));
+
+            //Way 2 - Dynamic Invoke.
+            foreach (Delegate del in addWords.GetInvocationList())
+            {
+                sentence += (string)del.DynamicInvoke();
+            }
+            Console.WriteLine(PrintTheSentence(sentence));
+
+
+            Console.ReadLine();
+        }
+        static string GetWord1()
+        {
+            return "Hello ";
+        }
+        static string GetWord2()
+        {
+            return "From ";
+        }
+        static string GetWord3()
+        {
+            return "Meir ";
+        }
+        static string PrintTheSentence(string sentence)
+        {
+            return sentence;
+        }
+    }
+}
